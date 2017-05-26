672c9de3882ec983e4629acdcf2ff200e2432120
renshen4322 <renshen4322@sina.com>
2017-05-26 12:57:22 +0800

    去安全分支

--- database/Index_init.sql
+++
@@ -1,2 +0,0 @@
-alter table community_user_images add index index_userid_isused (`user_id`,`is_used`);
-alter table community_category_relationships add index index_obhectid (`object_id`);
--- database/评论模块初始化数据.sql
+++
@@ -1,5 +0,0 @@
-#target_type
-INSERT community_target_type(`name`,`value`) values('works',1);
-INSERT community_target_type(`name`,`value`) values('product',2);
-INSERT community_target_type(`name`,`value`) values('news',3);
-INSERT community_target_type(`name`,`value`) values('comment',4);
